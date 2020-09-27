79d966d3df5d860468c46ae3c95c0bb6fdfcbac4
MrOhno <phamtechlong@gmail.com>
2020-09-27 21:16:18 +0700

    edited project 1 in async-js

--- asynchronous-javascript/practice-1/httprequest.js
+++ asynchronous-javascript/practice-1/httprequest.js
@@ -1,12 +1,25 @@
-//Make a fake REST API request using XMLHttpRequest Object in JS.
+const getComments = (callback) => {
+  //Make a fake REST API request using XMLHttpRequest Object in JS.
 
-const request = new XMLHttpRequest();
+  const request = new XMLHttpRequest();
 
-request.addEventListener("readystatechange", () => {
-  if (request.readyState === 4) {
-    console.log(request, request.responseText);
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback("Could not fetch data", undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/comments");
+  request.send();
+};
+
+getComments((err, data) => {
+  console.log("callback fired.");
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
   }
 });
-
-request.open('GET','https://jsonplaceholder.typicode.com/comments');
-request.send(); 
